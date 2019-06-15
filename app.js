83e4f35d0dda06f07537392d133935c95a3a707e
iamnikhil98 <32701827+iamnikhil98@users.noreply.github.com>
2019-06-15 09:45:14 +0530

    Added author routes

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ if(process.env.NODE_ENV !== 'production'){
 const express  = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors');
 const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
 app.set('view engine','ejs');
 app.set('views', __dirname + '/views');
 app.set('layout','layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({extended:true}))
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser : true});
 const db = mongoose.connection
@@ -23,7 +26,11 @@ db.once('open',()=> console.log('connected to mongoose'));
 
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
-app.listen(process.env.PORT || 5000,function(){
+
+
+
+app.listen(port, function(){
     console.log('Server is Started on port 5000');
 });
